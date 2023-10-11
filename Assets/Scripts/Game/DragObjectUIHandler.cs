b523ffbac69027176df77e6361da267a8369c979
CosplayingMagpie <128466629+CosplayingMagpie@users.noreply.github.com>
2023-10-11 12:25:41 +0200

    better outline shader

--- Assets/Scripts/Game/DragObjectUIHandler.cs
+++ Assets/Scripts/Game/DragObjectUIHandler.cs
@@ -2,11 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using DG.Tweening;
+using UnityEngine.UI;
 
 public class DragObjectUIHandler : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     private GameObject colorChangeButton;
     private GameObject resizeHandle;
+    private GameObject outline;
+
+    private Coroutine UICountdown;
+    private bool isUILingering;
+    private bool isUIVanishing;
+
+    private Tween fade1, fade2;
 
     [SerializeField] float UILingerTime;
 
@@ -16,26 +25,56 @@ public class DragObjectUIHandler : MonoBehaviour, IPointerEnterHandler, IPointer
         // Get UI Elements for the crafting Object
         colorChangeButton = transform.GetChild(0).gameObject;
         resizeHandle = transform.GetChild(1).gameObject;
-        DeactivateUI();
+        outline = transform.GetChild(2).gameObject;
+
+        colorChangeButton.GetComponent<Image>().DOFade(0, 1);
+        colorChangeButton.SetActive(false);
+
+        resizeHandle.GetComponent<Image>().DOFade(0, 1);
+        resizeHandle.SetActive(false);
+
+        //outline.GetComponent<Image>().DOFade(0, 1);
+        outline.SetActive(false);
+
 
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     private void DeactivateUI()
     {
-        colorChangeButton.SetActive(false);
-        resizeHandle.SetActive(false);
+        isUIVanishing = true;
+        fade1 = resizeHandle.GetComponent<Image>().DOFade(0, 0.5f);
+        fade2 =  colorChangeButton.GetComponent<Image>().DOFade(0, 0.5f).OnComplete(() =>
+        {
+            colorChangeButton.SetActive(false);
+            resizeHandle.SetActive(false);
+            outline.SetActive(false);
+            isUIVanishing = false;
+
+        });
+
     }
 
     private void ActivateUI()
     {
+        if(isUILingering)
+        {
+            StopCoroutine(UICountdown);
+            isUILingering = false;
+        }
+
+        if (isUIVanishing)
+        {
+            DOTween.Kill(fade1);
+            DOTween.Kill(fade2);
+            isUIVanishing = false;
+        }
         colorChangeButton.SetActive(true);
+        colorChangeButton.GetComponent<Image>().DOFade(1, 0.5f);
         resizeHandle.SetActive(true);
+        resizeHandle.GetComponent<Image>().DOFade(1, 0.5f);
+
+        outline.SetActive(true);
     }
 
     public void OnPointerEnter(PointerEventData eventData)
@@ -45,11 +84,12 @@ public class DragObjectUIHandler : MonoBehaviour, IPointerEnterHandler, IPointer
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        StartCoroutine(ShowUIForAWhileLonger());
+       UICountdown =  StartCoroutine(ShowUIForAWhileLonger());
     }
 
     private IEnumerator ShowUIForAWhileLonger()
     {
+        isUILingering = true;
         yield return new WaitForSeconds(UILingerTime);
         DeactivateUI();
     }
