2e5c20b9a4bb50c3983a33eb927a51b495ca9db1
CosplayingMagpie <128466629+CosplayingMagpie@users.noreply.github.com>
2023-10-16 14:23:51 +0200

    working on making the resize handler not move out of bounds

--- Assets/Scripts/Game/ResizeHandler.cs
+++ Assets/Scripts/Game/ResizeHandler.cs
@@ -1,11 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class ResizeHandler : MonoBehaviour
+public class ResizeHandler : MonoBehaviour, IDragHandler, IEndDragHandler
 {
+    private DragManager _manager = null;
+
+    private Vector2 _centerPoint;
+    private Vector2 _worldCenterPoint => transform.TransformPoint(_centerPoint);
 
     private Transform parentTransform;
+    private RectTransform parentRect;
+
     private Vector3 initialSize;
     private float originalDistance;
     private Vector3 initialPosition;
@@ -15,11 +22,17 @@ public class ResizeHandler : MonoBehaviour
     private Vector3 originalDirection;
     private Vector3 newDirection;
 
+    public bool canMove;
+
+    private Rect _boundingBox;
+
 
 
     private void Start()
     {
+        canMove = true;
         parentTransform = transform.parent;
+        parentRect = parentTransform.GetComponent<RectTransform>();
 
         //setting up the sizing variables
         originalDistance = Vector3.Distance(parentTransform.position, transform.position);
@@ -28,45 +41,125 @@ public class ResizeHandler : MonoBehaviour
 
         //setting up the rotation Variables
         originalDirection = parentTransform.position - transform.position;
+
+        SetBoundingBoxRect(parentRect);
     }
 
 
 
 
-    // Update is called once per frame
-    void Update()
-    {
-        //Calculates the new size depending on the distance dragged
-        distance = Vector3.Distance(parentTransform.position, transform.position);
-        float newScale = distance / originalDistance;
-        newScale = Mathf.Clamp(newScale, 0.5f, 1.5f);
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    //Calculates the new size depending on the distance dragged
+    //    distance = Vector3.Distance(parentTransform.position, transform.position);
+    //    float newScale = distance / originalDistance;
 
-        //sets the new size
-        parentTransform.localScale = new Vector3 (1, 1, 1) * newScale;
+    //    if(newScale <= 0.5f || newScale >= 1.5f)
+    //    {
+    //        newScale = Mathf.Clamp(newScale, 0.5f, 1.5f);
+    //        canMove = false;
 
-        //calculates the new rotation
-        newDirection = parentTransform.position - transform.position;
-        float angle = Vector3.Angle(originalDirection, newDirection);
+    //    }
 
-        //sets the new rotation depending on y-value
-        if (transform.position.y < parentTransform.position.y)
-        {
-            parentTransform.rotation = Quaternion.Euler(0, 0, angle);
-            transform.rotation = Quaternion.Euler(0, 0, angle);
-        }
+    //    else
+    //    {
+    //        canMove = true;
+    //    }
 
-        else
-        {
-            parentTransform.rotation = Quaternion.Euler(0, 0, -angle);
-            transform.rotation = Quaternion.Euler(0, 0, -angle);
-        }
+    //    //sets the new size
+    //    parentTransform.localScale = new Vector3 (1, 1, 1) * newScale;
+
+    //    //calculates the new rotation
+    //    newDirection = parentTransform.position - transform.position;
+    //    float angle = Vector3.Angle(originalDirection, newDirection);
+
+    //    //sets the new rotation depending on y-value
+    //    if (transform.position.y < parentTransform.position.y)
+    //    {
+    //        parentTransform.rotation = Quaternion.Euler(0, 0, angle);
+    //        transform.rotation = Quaternion.Euler(0, 0, angle);
+    //    }
+
+    //    else
+    //    {
+    //        parentTransform.rotation = Quaternion.Euler(0, 0, -angle);
+    //        transform.rotation = Quaternion.Euler(0, 0, -angle);
+    //    }
         
 
+    //}
+
+    public bool IsWithinBounds(Vector2 position)
+    {
+        return _boundingBox.Contains(position);
     }
 
 
+    //private void OnMouseUp()
+    //{
+    //    //resets position of handle and parent size, so next resizing will be to new scale
+    //    initialPosition = transform.position;
+    //    initialSize = parentTransform.localScale;
 
-    private void OnMouseUp()
+    //    //resetting up the rotation Variables
+    //    originalDirection = parentTransform.position - transform.position;
+    //}
+
+    private void Awake()
+    {
+        //_manager = GetComponentInParent<DragManager>();
+        _centerPoint = (transform as RectTransform).rect.center;
+    }
+
+    //public void OnBeginDrag(PointerEventData eventData)
+    //{
+    //    //_manager.RegisterDraggedObject(this);
+    //}
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (IsWithinBounds(_worldCenterPoint + eventData.delta))
+        {
+            transform.Translate(eventData.delta, Space.World);
+
+            //Calculates the new size depending on the distance dragged
+            distance = Vector3.Distance(parentTransform.position, transform.position);
+            float newScale = distance / originalDistance;
+
+
+            newScale = Mathf.Clamp(newScale, 0.5f, 1.5f);
+
+
+
+
+            //sets the new size
+            parentTransform.localScale = new Vector3(1, 1, 1) * newScale;
+
+            //calculates the new rotation
+            newDirection = parentTransform.position - transform.position;
+            float angle = Vector3.Angle(originalDirection, newDirection);
+
+            //sets the new rotation depending on y-value
+            if (transform.position.y < parentTransform.position.y)
+            {
+                parentTransform.rotation = Quaternion.Euler(0, 0, angle);
+                transform.rotation = Quaternion.Euler(0, 0, angle);
+            }
+
+            else
+            {
+                parentTransform.rotation = Quaternion.Euler(0, 0, -angle);
+                transform.rotation = Quaternion.Euler(0, 0, -angle);
+            }
+            SetBoundingBoxRect(parentRect);
+        }
+
+
+        
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
     {
         //resets position of handle and parent size, so next resizing will be to new scale
         initialPosition = transform.position;
@@ -74,7 +167,24 @@ public class ResizeHandler : MonoBehaviour
 
         //resetting up the rotation Variables
         originalDirection = parentTransform.position - transform.position;
+
+        SetBoundingBoxRect(parentRect);
     }
+
+
+    private void SetBoundingBoxRect(RectTransform rectTransform)
+    {
+        var corners = new Vector3[4];
+        rectTransform.GetWorldCorners(corners);
+        var position = corners[0];
+
+        Vector2 size = new Vector2(
+            rectTransform.lossyScale.x * rectTransform.rect.size.x,
+            rectTransform.lossyScale.y * rectTransform.rect.size.y);
+
+        _boundingBox = new Rect(position, size);
+    }
+
 
 }
 
